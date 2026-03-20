5f9ab28fb1774dcc96de1829ed9be3a29dea60d1
alternator0 <konrad.fraczek@protonmail.com>
2026-03-20 09:59:09 +0100

    Fixed run_sim.sh

--- run_sim.sh
+++ run_sim.sh
@@ -1,5 +1,8 @@
-cd build 
-make
+#!/bin/bash
+
+cmake -S . -B ./build/
+cmake --build ./build/ -j $(nproc)
+cd ./build/
 time ./rozwielitki simulation.mac | grep masa
 cd ..
 ./licz_energie.sh ./build/WynikiSymulacji.root
